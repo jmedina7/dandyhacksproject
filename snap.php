79a78818c03e4f2cd932efa54979c8cd2f85cb12
serenaflint <44509543+serenaflint@users.noreply.github.com>
2018-10-27 14:14:15 -0400

    Update snap.php

--- snap.php
+++ snap.php
@@ -10,10 +10,27 @@ include('inc/html-top.php'); ?>
 				<h2> Eating on a budget </h2>
 			</header>
 			<main>
-				<h2>Information about Supplemental Nutrition Assistance Program (SNAP)</h2>
+				<h2>IThe Supplemental Nutrition Assistance Program (SNAP)</h2>
 				<p>The Supplemental Nutrition Assistance Program (SNAP) is a federal program dedicated to providing supplemental income to low-income families, senior citizens, the disabled, and other families in need. Eligibility for SNAP is primarily dependent on household income, but in certain circumstances the program will hear other cases. In addition to income requirements, New York also requires that the primary applicant works an average of 20 hrs/week. To find a chart of income eligibility based on family size in NY, click <a href="https://www.ny.gov/services/apply-snap">here</a>.</p>
+
 				<h2>Applying for SNAP</h2>
-				<p>Nullam vel elit augue. In quis facilisis odio. Nunc elit nulla, volutpat eu ante non, malesuada faucibus nisi. Fusce vel auctor ipsum, et elementum sapien. Aenean finibus ipsum accumsan lorem finibus, quis pretium nisl vehicula. Suspendisse potenti.</p>
+				<p>Applications can be submitted online 24/7 on NY's website, in person, or through mail.</p>
+
+				<h3>Online</h3>
+				<p>To apply for SNAP online in New York, you can visit the myBenefits page by searching or clicking <a href="https://mybenefits.ny.gov/mybenefits/begin">here</a>.</p>
+
+				<h3>In Person</h3>
+				<p>For applicants in Rochester, you can apply in person from 8AM-4PM at Monroe County Human Services:</p>
+				<ul>
+					<li>111 Westfall Road, Rochester NY, 14620</li>
+					<li>691 St. Paul Street, Rochester, NY 14605</li>
+				</ul>
+				<p>For applicants outside of Rochester, you can apply at your local SNAP or Social Services office, or call your state SNAP hotline. A list of each state's hotline can be found <a href="https://www.fns.usda.gov/snap/state-informationhotline-numbers">here</a>.</p>
+
+				<h3>Mail</h3>
+				<p>To apply through mail, applications can be mailed to your local SNAP office.</p>
+
+				<h2>Where to Use SNAP</h2>
 				<p>Proin ipsum nisi, cursus vitae molestie sed, imperdiet vitae dui. Vestibulum imperdiet luctus leo, eu hendrerit neque dictum non. Donec tincidunt ut erat sit amet suscipit. Nam massa ante, cursus a risus vel, elementum egestas nibh. Sed blandit, purus semper faucibus laoreet, augue purus porttitor est, id gravida velit mi vitae enim. Sed sollicitudin at felis a sagittis.</p>
 			</main>
 			<?php include('inc/scripts.php'); ?>
