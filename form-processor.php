4f6f0e855a4e6c4fce0203226042ecbfad7e6c13
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-10-27 10:36:03 -0400

    i dunno

--- form-processor.php
+++ form-processor.php
@@ -6,34 +6,14 @@
 // set some variables
 $emailFrom = "jmedina7@u.rochester.edu";
 $emailTo = "jmedina7@u.rochester.edu";
-$subject = "Lab 11 - HTML Forms";
+$subject = "Dandy Hacks";
 
 // for the following lines of code, grab the data being passed 
 //   from the method="post" in the HTML form and hold it in a variable
 
-$name= Trim(stripslashes($_POST['name'])); 
-$email = Trim(stripslashes($_POST['email'])); 
-$tel = Trim(stripslashes($_POST['tel'])); 
+$budget = Trim(stripslashes($_POST['budget'])); 
 // ...used more input fields? Then copy these lines (above)
 //   and make more
-
-$chaser = $_POST['chaser']; 
-$beater = $_POST['beater']; 
-$keeper = $_POST['keeper']; 
-$seeker = $_POST['seeker']; 
-// ...used more checkboxes? Then copy these lines (above)
-//   and make more
-
-
-// for your radio buttons:
-$house = $_POST['house']; 
-// notice: no matter how many radio buttons, there's only one answer
-
-
-// for your textarea:
-$comments = Trim(stripslashes($_POST['comments']));
-
-
 
 // **********************************
 // SECTION TWO: build the email body
@@ -42,52 +22,11 @@ $comments = Trim(stripslashes($_POST['comments']));
 $body = ""; // initialize the variable, then start concatenating
 			// backslash-n means new-line in emails
 
-$body .= "Name: \n"; //...a label
-$body .= $name;	  //...a variable
-$body .= "\n\n";
-
-$body .= "Email: \n"; //...a label
-$body .= $email;	  //...a variable
-$body .= "\n\n";
-
-$body .= "Telephone: \n"; //...a label
-$body .= $tel;	  //...a variable
+$body .= "Budget: \n"; //...a label
+$body .= $budget;	  //...a variable
 $body .= "\n\n";
 // ...used more input fields? Then copy these lines (above)
 //   and make more
-
-
-
-// Do your checkboxes this way...
-$body .= "What Quidditch positions would you be interested in playing? \n"; 	 	// a heading for your checkbox section
-
-if (isset($chaser)) {		// a checkbox variable
-	$body .= $chaser . "\n";	// the same checkbox variable
-}
-if (isset($beater)) {		// a checkbox variable
-	$body .= $beater . "\n";	// the same checkbox variable
-}
-if (isset($keeper)) {		// a checkbox variable
-	$body .= $keeper . "\n";	// the same checkbox variable
-}
-if (isset($seeker)) {		// a checkbox variable
-	$body .= $seeker . "\n";	// the same checkbox variable
-}
-$body .= "\n";
-// ...used more checkbox fields? Then copy these lines (above)
-//   and make more
-
-
-
-// This is for your radio buttons (always just one answer)...
-$body .= "What is your Hogwarts House? \n";		// a heading for your radio button section
-$body .= $house;			// the (one) radio button variable
-$body .= "\n\n";
-
-// This is for your TEXTAREA
-$body .= "What has brought you to this website? \n";		// a heading for your text area
-$body .= $comments;			// the variable for your text area
-$body .= "\n";
 
 
 // **********************************
