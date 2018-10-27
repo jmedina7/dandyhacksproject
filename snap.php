34bef8aa42bcff87cb01b70866a63f84d6873c30
serenaflint <44509543+serenaflint@users.noreply.github.com>
2018-10-27 15:31:47 -0400

    Update snap.php

--- snap.php
+++ snap.php
@@ -9,11 +9,36 @@ include('inc/html-top.php'); ?>
 				<h1> Roc Recipes </h1>
 			</header>
 			<main>
-				<h2>IThe Supplemental Nutrition Assistance Program (SNAP)</h2>
+				<h2>The Supplemental Nutrition Assistance Program (SNAP)</h2>
 				<p>The Supplemental Nutrition Assistance Program (SNAP) is a federal program dedicated to providing supplemental income to low-income families, senior citizens, the disabled, and other families in need. Eligibility for SNAP is primarily dependent on household income, but in certain circumstances the program will hear other cases. In addition to income requirements, New York also requires that the primary applicant works an average of 20 hrs/week. To find a chart of income eligibility based on family size in NY, click <a href="https://www.ny.gov/services/apply-snap">here</a>.</p>
+				<p>SNAP benefits are either stored directly on an EBT/SNAP card, or the balance is recorded on a server run by the state of residence. These cards function similarly to debit cards with a pin, and at a set date each month a balance will be added to the account, depending on the family size and demonstrated need. Purchases using SNAP benefits will be limited to only food and food related itemss. Examples of purchaseable and non-purchaseable items include: </p>
+				<h3>Purchaseable</h3>
+				<ul>
+					<li>Produce</li>
+					<li>Non-Perishables</li>
+					<li>Non-Alcoholic Beverages</li>
+					<li>Food-Producing Seeds</li>
+					<li>Spices & Ingredients</li>
+					<li>Bagged Ice</li>
+				</ul>
+
+				<h3>Non-Purchaseable</h3>
+				<ul>
+					<li>Alcohol</li>
+					<li>Pet Food</li>
+					<li>Toiletrries</li>
+					<li>Vitamins</li>
+					<li>Preapred, Hot Foods (ex. pizza on a front display)</li>
+				</ul>
 
 				<h2>Applying for SNAP</h2>
-				<p>Applications can be submitted online 24/7 on NY's website, in person, or through mail.</p>
+				<p>Applications can be submitted online 24/7 on NY's website, in person, or through mail. To apply, make sure you have ready: </p>
+				<ul>
+					<li>Proof of Income</li>
+					<li>Proof of Identity (with photo)</li>
+					<li>Social Security Number</li>
+					<li>Utility Bills/Medical Bills/Child Support Documents (known expenses)</li>
+				</ul>
 
 				<h3>Online</h3>
 				<p>To apply for SNAP online in New York, you can visit the myBenefits page by searching or clicking <a href="https://mybenefits.ny.gov/mybenefits/begin">here</a>.</p>
@@ -21,7 +46,7 @@ include('inc/html-top.php'); ?>
 				<h3>In Person</h3>
 				<p>For applicants in Rochester, you can apply in person from 8AM-4PM at Monroe County Human Services:</p>
 				<ul>
-					<li>111 Westfall Road, Rochester NY, 14620</li>
+					<li>111 Westfall Road, Rochester, NY 14620</li>
 					<li>691 St. Paul Street, Rochester, NY 14605</li>
 				</ul>
 				<p>For applicants outside of Rochester, you can apply at your local SNAP or Social Services office, or call your state SNAP hotline. A list of each state's hotline can be found <a href="https://www.fns.usda.gov/snap/state-informationhotline-numbers">here</a>.</p>
@@ -30,7 +55,8 @@ include('inc/html-top.php'); ?>
 				<p>To apply through mail, applications can be mailed to your local SNAP office.</p>
 
 				<h2>Where to Use SNAP</h2>
-				<p>Proin ipsum nisi, cursus vitae molestie sed, imperdiet vitae dui. Vestibulum imperdiet luctus leo, eu hendrerit neque dictum non. Donec tincidunt ut erat sit amet suscipit. Nam massa ante, cursus a risus vel, elementum egestas nibh. Sed blandit, purus semper faucibus laoreet, augue purus porttitor est, id gravida velit mi vitae enim. Sed sollicitudin at felis a sagittis.</p>
+				<p>Most larger chain grocery and department stores accept SNAP benefits! As the program expands and becomes more accesible for businesses, more and more stores can offer support for SNAP. Many smaller stores and gas stations are able to offer support for SNAP benefits, however these transactions may take longer for older systems. Most stores that can accept SNAP will place a sign in their window with the SNAP or EBT logo, however stores that do not post these signs may still accept such payments.</p>
+				<p>Due to the rise of online retail, many large retailers such as Amazon have been pushing to accept SNAP benefits as a method of payment for food products, however this program has not yet been implemented. As of now, Amazon will offer a discounted subscription to Amazon Prime for customers with an EBT or Medicaid card.</p>
 			</main>
 			<?php include('inc/scripts.php'); ?>
 		</div>
