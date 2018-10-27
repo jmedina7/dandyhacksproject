00477f2b998b5449c6497280e1638ca3c5156bc1
serenaflint <44509543+serenaflint@users.noreply.github.com>
2018-10-27 13:45:46 -0400

    Update snap.php

--- snap.php
+++ snap.php
@@ -11,7 +11,8 @@ include('inc/html-top.php'); ?>
 			</header>
 			<main>
 				<h2>Information about Supplemental Nutrition Assistance Program (SNAP)</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor mi, fringilla in metus vel, efficitur imperdiet mauris. Nulla accumsan pretium dignissim. Maecenas a euismod quam, vel convallis magna.</p>
+				<p>The Supplemental Nutrition Assistance Program (SNAP) is a federal program dedicated to providing supplemental income to low-income families, senior citizens, the disabled, and other families in need. Eligibility for SNAP is primarily dependent on household income, but in certain circumstances the program will hear other cases. In addition to income requirements, New York also requires that the primary applicant works an average of 20 hrs/week. To find a chart of income eligibility based on family size in NY, click <a href="https://www.ny.gov/services/apply-snap">here</a>.</p>
+				<h2>Applying for SNAP</h2>
 				<p>Nullam vel elit augue. In quis facilisis odio. Nunc elit nulla, volutpat eu ante non, malesuada faucibus nisi. Fusce vel auctor ipsum, et elementum sapien. Aenean finibus ipsum accumsan lorem finibus, quis pretium nisl vehicula. Suspendisse potenti.</p>
 				<p>Proin ipsum nisi, cursus vitae molestie sed, imperdiet vitae dui. Vestibulum imperdiet luctus leo, eu hendrerit neque dictum non. Donec tincidunt ut erat sit amet suscipit. Nam massa ante, cursus a risus vel, elementum egestas nibh. Sed blandit, purus semper faucibus laoreet, augue purus porttitor est, id gravida velit mi vitae enim. Sed sollicitudin at felis a sagittis.</p>
 			</main>
