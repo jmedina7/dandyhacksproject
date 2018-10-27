be93823a9bd758911c9ea131d20b59fc85a0844a
Sarah Field <sgfield15@gmail.com>
2018-10-27 11:34:52 -0400

    sf - fixed cors issue

--- js/recipes.js
+++ js/recipes.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    var x = document.getElementById("foods").value;
-    console.log(x);
-    fetch('http://www.recipepuppy.com/api/?i=' + x + '&p=3')
-    .then(function(response) {
-      console.log(response)
-      response.json().then((data) => {
-            for (var i = 0; i < data["results"].length; i++) {
-              var idname = "recipe" + (i+1);
-              document.getElementById(idname).innerHTML = data["results"][i]["title"];
-              document.getElementById(idname).innerHTML = data["results"][i]["title"];
-              document.getElementById(idname).innerHTML = data["results"][i]["href"];
-            }
-        });
-    })
-    .catch(function(error) {
-      console.log('Looks like there was a problem: \n', error);
-    });
+$(document).ready(function() {
+  $("button").click(function() {
+      var x = document.getElementById("foods").value;
+      console.log(x);
+      var proxy = 'https://cors-anywhere.herokuapp.com/';
+      fetch(proxy + 'http://www.recipepuppy.com/api/?i=' + x + '&p=1')
+          .then(function(response) {
+              console.log(response)
+              response.json().then((data) => {
+                  console.log(data)
+                  for (var i = 0; i < data["results"].length; i++) {
+                      var idname = "recipe" + (i + 1);
+                      document.getElementById(idname).innerHTML = data["results"][i]["title"];
+                  }
+              });
+          })
+          .catch(function(error) {
+              console.log('Looks like there was a problem: \n', error);
+          });
   });
 });
