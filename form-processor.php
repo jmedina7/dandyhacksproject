e1800f42cb66b506c15c49dc49a21d3131c473b8
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-10-27 11:45:54 -0400

    Update form-processor.php

--- form-processor.php
+++ form-processor.php
@@ -10,10 +10,14 @@ $subject = "Dandy Hacks";
 
 // for the following lines of code, grab the data being passed 
 //   from the method="post" in the HTML form and hold it in a variable
-
+$name= Trim(stripslashes($_POST['name'])); 
+$email = Trim(stripslashes($_POST['email']));
 $budget = Trim(stripslashes($_POST['budget'])); 
 // ...used more input fields? Then copy these lines (above)
 //   and make more
+
+// for the textarea:
+$userrecipe = Trim(stripslashes($_POST['userrecipe']));
 
 // **********************************
 // SECTION TWO: build the email body
@@ -22,11 +26,26 @@ $budget = Trim(stripslashes($_POST['budget']));
 $body = ""; // initialize the variable, then start concatenating
 			// backslash-n means new-line in emails
 
+$body .= "Name: \n"; //...a label
+$body .= $name;	  //...a variable
+$body .= "\n\n";
+
+$body .= "Email: \n"; //...a label
+$body .= $email;	  //...a variable
+$body .= "\n\n";
+
 $body .= "Budget: \n"; //...a label
 $body .= $budget;	  //...a variable
 $body .= "\n\n";
 // ...used more input fields? Then copy these lines (above)
 //   and make more
+
+// This is for the TEXTAREA
+$body .= "Recipe: \n";		// a heading for the text area
+$body .= $userrecipe;			// the variable for the text area
+$body .= "\n";
+
+
 
 
 // **********************************
