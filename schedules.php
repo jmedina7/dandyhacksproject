aded9f8cb790ce737a13975a4b251b2036c3a53d
mrobin36 <44514026+mrobin36@users.noreply.github.com>
2018-10-27 17:56:17 -0500

    bus fare added

--- schedules.php
+++ schedules.php
@@ -10,7 +10,7 @@ include('inc/html-top.php'); ?>
 			</header>
 			<main>
 				<h2>Bus Schedules</h2>
-				<p>RTS shows all bus routes inthe Downtown Rochester and Monroe County areas. You can see this map <a href="https://www.myrts.com/Portals/0/Schedules/RTS-System-Map-April-3-2017.pdf">here</a>. </p>
+				<p>RTS shows all bus routes inthe Downtown Rochester and Monroe County areas. You can see this map <a href="https://www.myrts.com/Portals/0/Schedules/RTS-System-Map-April-3-2017.pdf">here</a>. Fare is $1 for children over 12 and for adults, $0.50 for children aged 6-11, and free for children 5 and under.</p>
 				<p>If you want to find the best route from your location to a nearby grocery store or other location, you can find a specific schedule <a href="https://www.myrts.com/Maps-Schedules/RTS-Monroe/Schedule-pdfs">here</a>. Google Transit Service is also an excellent tool to find transportation options in the Rochester area, and can be found <a href="http://maps.google.com/landing/transit/index.html">here</a>.</p>
 				<p>Medical Motor Services provides transportation to those with special needs in the Rochester area. If you are a registered user, or would like to become a registered user, you can learn more <a href="https://www.medicalmotors.org/">here</a>.</p>
 			</main>
