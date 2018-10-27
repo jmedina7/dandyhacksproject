286192c3d405a5044930b310c3b2ea6f2d953520
mrobin36 <44514026+mrobin36@users.noreply.github.com>
2018-10-27 16:56:29 -0500

    BUS

--- schedules.php
+++ schedules.php
@@ -11,7 +11,7 @@ include('inc/html-top.php'); ?>
 			<main>
 				<h2>Bus Schedules</h2>
 				<p>RTS shows all bus routes inthe Downtown Rochester and Monroe County areas. You can see this map <a href="https://www.myrts.com/Portals/0/Schedules/RTS-System-Map-April-3-2017.pdf">here</a></p>
-				<p>If you want to find the best route from your location to a nearby grocery store or other location, you can find a specific schedule <a href="https://www.myrts.com/Maps-Schedules/RTS-Monroe/Schedule-pdfs">here</a>. <a href="http://maps.google.com/landing/transit/index.html ">Google Transit Service</a> </p> is also an excellent tool to find transportation information in the Rochester area.
+				<p>If you want to find the best route from your location to a nearby grocery store or other location, you can find a specific schedule <a href="https://www.myrts.com/Maps-Schedules/RTS-Monroe/Schedule-pdfs">here</a>. <a href="http://maps.google.com/landing/transit/index.html ">Google Transit Service</a> is also an excellent tool to find transportation information in the Rochester area.</p>
 				<p>Medical Motor Services provides transportation to those with special needs in the Rochester area. If you are a registered user, or would like to become a registered user, you can learn more <a href="https://www.medicalmotors.org/">here</a></p>
 			</main>
 			<?php include('inc/scripts.php'); ?>
