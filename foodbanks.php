48ad8588d26f6dc754fbfd20cb0068f6f320f9dd
serenaflint <44509543+serenaflint@users.noreply.github.com>
2018-10-27 16:15:17 -0400

    food bank and text stuff

--- foodbanks.php
+++ foodbanks.php
@@ -10,9 +10,34 @@ include('inc/html-top.php'); ?>
 			</header>
 			<main>
 				<h2>Local Food Banks</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor mi, fringilla in metus vel, efficitur imperdiet mauris. Nulla accumsan pretium dignissim. Maecenas a euismod quam, vel convallis magna.</p>
-				<p>Nullam vel elit augue. In quis facilisis odio. Nunc elit nulla, volutpat eu ante non, malesuada faucibus nisi. Fusce vel auctor ipsum, et elementum sapien. Aenean finibus ipsum accumsan lorem finibus, quis pretium nisl vehicula. Suspendisse potenti.</p>
-				<p>Proin ipsum nisi, cursus vitae molestie sed, imperdiet vitae dui. Vestibulum imperdiet luctus leo, eu hendrerit neque dictum non. Donec tincidunt ut erat sit amet suscipit. Nam massa ante, cursus a risus vel, elementum egestas nibh. Sed blandit, purus semper faucibus laoreet, augue purus porttitor est, id gravida velit mi vitae enim. Sed sollicitudin at felis a sagittis.</p>
+				<p>Here are a list of local food pantries that can further help families in Rochester.</p>
+				
+				<h3>Bethany House</h3>
+				<ul>
+					<li>Address: 1111 Joseph Ave, Rochester, NY 14621</li>
+					<li>Phone: (585) 275-5151</li>
+				</ul>
+
+				<h3>Brighton Food Cupboard</h3>
+				<p>Offers other items in addition to food, including clothing or cookware. Typical clients inclue those with a sudden loss of income, but anyone is welcome to call</p>
+				<ul>
+					<li>Address: 220 Idlewood Rd, Rochester, NY 14618</li>
+					<li>Phone: (585) 271-5355</li>
+				</ul>
+
+				<h3>Cameron Community Ministries Food Pantry</h3>
+				<ul>
+					<li>Address: 48 Cameron Street, Rochester, New York 14606</li>
+					<li>Phone: (585) 254-2697</li>
+				</ul>
+
+				<h3>Catholic Family Center, Community Resource Services Program</h3>
+				<p>The Monroe County Catholic service administers several social service programs in addition to food pantries. They also provide clothing and general counseling and case management for families in need</p>
+				<ul>
+					<li>Address: 30 Hart Street Entrance #5 Rochester, New York 14605</li>
+					<li>Phone: (585) 546-7220</li>
+				</ul>
+
 			</main>
 			<?php include('inc/scripts.php'); ?>
 		</div>
