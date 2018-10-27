f1cbdb9e791df65bda80bb23379a3a55370d93d6
Sarah Field <sgfield15@gmail.com>
2018-10-27 09:56:12 -0400

    sf - added some ajax

--- inc/nav.php
+++ inc/nav.php
@@ -4,4 +4,7 @@
 		<li><a href="recipes.php">Recipes</a></li>
 		<li><a href="about.php">About Our Team</a></li>
 	</ul>
+	<div id="searchbar">
+		<input type="text" id="foods" value="Enter 1 or 2 foods">
+	</div>
 </nav>
--- index.php
+++ index.php
@@ -5,13 +5,43 @@ include('inc/html-top.php'); ?>
 	<body>
 		<div class = trans>
 			<header>
-
 				<?php include ('inc/nav.php'); ?>
 				<h1> Roc Recipes </h1>
 				<caption> Eating on a budget </caption>
 			</header>
 			<main>
 				<P> THIS IS SOME FILLER TEXT BB </P>
+				<button>Click me</button>
+				<div id="recipe1">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe2">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe3">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe4">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe5">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe6">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe7">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe8">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe9">
+					<p>Recipe name</p>
+				</div>
+				<div id="recipe10">
+					<p>Recipe name</p>
+				</div>
 			</main>
 			<?php include('inc/scripts.php'); ?>
 		</div>
