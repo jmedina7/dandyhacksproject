354d2e933c4d654b06e519097258ed2a8877840e
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-10-27 17:12:50 -0400

    added resources

---
+++ resources.php
@@ -0,0 +1,18 @@
+<?php
+$pageTitle = "Roc Recipes";
+include('inc/html-top.php'); ?>
+
+  <body>
+    <div class = trans>
+      <header>
+        <?php include ('inc/nav.php'); ?>
+        <h1> Roc Recipes </h1>
+      </header>
+      <main>
+      <h2>Resources</h2>
+      <p>
+   	</main>
+    <?php include "inc/scripts.inc"; ?>
+  </div>
+  </body>
+</html>
