59ae3d3b8b959b5aa9b8429a88747d022a1e1aab
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-10-27 00:18:12 -0400

    more

--- index.php
+++ index.php
@@ -3,19 +3,18 @@ $pageTitle = "Roc Recipes";
 include('inc/html-top.php'); ?>
 
 	<body>
+		<div class = trans>
+			<header>
 
-		<header>
-
-			<?php include ('inc/nav.php'); ?>
-			<h1> Roc Recipes </h1>
-			<caption> Eating on a budget </caption>
-
-		</header>
-
-
-
-		<?php include('inc/scripts.php'); ?>
-
+				<?php include ('inc/nav.php'); ?>
+				<h1> Roc Recipes </h1>
+				<caption> Eating on a budget </caption>
+			</header>
+			<main>
+				<P> THIS IS SOME FILLER TEXT BB </P>
+			</main>
+			<?php include('inc/scripts.php'); ?>
+		</div>
 	</body>
 
 </html>
