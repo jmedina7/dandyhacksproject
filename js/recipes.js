502fc6bac0b03ac2fea99f7b9f5a392b39dfc593
Sarah Field <sgfield15@gmail.com>
2018-10-27 18:36:18 -0400

    sf - more coolness

--- js/recipes.js
+++ js/recipes.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
                   for (var i = 0; i < data["results"].length; i++) {
                       var idname = "recipe" + (i + 1);
                     if (data["results"][i]["thumbnail"]) {
-                        document.getElementById(idname).innerHTML = "<img src=" + data["results"][i]["thumbnail"] + " style=\"width:200px;height:200px;border:0\" class=\"image\">";
+                        document.getElementById(idname).innerHTML = "<img src=" + data["results"][i]["thumbnail"] + " style=\"width:150px;height:150px;border:0\" class=\"image\">";
                     } else {
-                        document.getElementById(idname).innerHTML = "<img src=https://i.pinimg.com/originals/e0/9f/ff/e09fff0a2bc81e18f177f6549e402f33.gif style=\"width:200px;height:200px;border:0\" class=\"image\">";
+                        document.getElementById(idname).innerHTML = "<img src=https://i.pinimg.com/originals/e0/9f/ff/e09fff0a2bc81e18f177f6549e402f33.gif style=\"width:150px;height:150px;border:0\" class=\"image\">";
                     }
                       document.getElementById((i+1)+"text").innerHTML = "<a href=" + data["results"][i]["href"] + ">" + data["results"][i]["title"] + "</a>";
                       document.getElementById(idname).target = "_blank";
