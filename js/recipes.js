60ec1ec8101a7e62d8e078a148ea2d5325886500
Sarah Field <sgfield15@gmail.com>
2018-10-27 22:57:37 -0400

    sf - something is diff??

--- js/recipes.js
+++ js/recipes.js
@@ -3,31 +3,33 @@ $(document).ready(function() {
       var x = document.getElementById("foods").value;
       x = x.replace(" ", ",")
       var y = document.getElementById("budgetSlider").value;
-      fetch('http://www.recipepuppy.com/api/?i=' + x + '&p=' + Math.floor(Math.random() * 10) + 1)
-          .then(function(response) {
-              console.log(response)
-              response.json().then((data) => {
-                  console.log(data)
-                  if (data["results"].length == 0) {
-                    document.getElementById("resultText").innerHTML = "There were no recipes for " + x + " under $" + y
-                  } else {
-                    document.getElementById("resultText").innerHTML = "We found " + data["results"].length + " results for " + x + " under $" + y
-                  for (var i = 0; i < data["results"].length; i++) {
-                      var idname = "recipe" + (i + 1);
-                    if (data["results"][i]["thumbnail"]) {
-                        document.getElementById(idname).innerHTML = "<img src=" + data["results"][i]["thumbnail"] + " style=\"width:150px;height:150px;border:0\" class=\"image\">";
+      for (var q = 1; q < 11; q++) {
+        fetch('http://www.recipepuppy.com/api/?i=' + x + '&p=' + Math.floor(Math.random() * 10) + 1)
+            .then(function(response) {
+                console.log(response)
+                response.json().then((data) => {
+                    console.log(data)
+                    if (data["results"].length == 0) {
+                        document.getElementById("resultText").innerHTML = "There were no recipes for " + x + " under $" + y
                     } else {
-                        document.getElementById(idname).innerHTML = "<img src=https://i.pinimg.com/originals/e0/9f/ff/e09fff0a2bc81e18f177f6549e402f33.gif style=\"width:150px;height:150px;border:0\" class=\"image\">";
+                        document.getElementById("resultText").innerHTML = "We found " + data["results"].length + " results for " + x + " under $" + y
+                    for (var i = 0; i < data["results"].length; i++) {
+                        var idname = "recipe" + (i + 1);
+                        if (data["results"][i]["thumbnail"]) {
+                            document.getElementById(idname).innerHTML = "<img src=" + data["results"][i]["thumbnail"] + " style=\"width:150px;height:150px;border:0\" class=\"image\">";
+                        } else {
+                            document.getElementById(idname).innerHTML = "<img src=https://i.pinimg.com/originals/e0/9f/ff/e09fff0a2bc81e18f177f6549e402f33.gif style=\"width:150px;height:150px;border:0\" class=\"image\">";
+                        }
+                        document.getElementById((i+1)+"text").innerHTML = "<a href=" + data["results"][i]["href"] + ">" + data["results"][i]["title"] + "</a>";
+                        document.getElementById(idname).target = "_blank";
                     }
-                      document.getElementById((i+1)+"text").innerHTML = "<a href=" + data["results"][i]["href"] + ">" + data["results"][i]["title"] + "</a>";
-                      document.getElementById(idname).target = "_blank";
-                  }
-                }
-              });
-          })
-          .catch(function(error) {
-              console.log('Looks like there was a problem: \n', error);
-          });
+                    }
+                });
+            })
+            .catch(function(error) {
+                console.log('Looks like there was a problem: \n', error);
+            });
+        }
   });
   $('#foods').keypress(function(e){
     if(e.which == 13){//Enter key pressed
