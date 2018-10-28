1f8364c8992b0538db294a7003ca2f372e3e4c78
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-10-27 23:27:41 -0400

    sizing

--- mission.php
+++ mission.php
@@ -6,7 +6,7 @@ include('inc/html-top.php'); ?>
 				<h2>Our Mission</h2>
 				<p>Rochester contains communities with some of the highest food insecurity rates in New York State. As a whole, Rochester has a food insecurity rate of 25.9%, higher than any other in the state. Food insecurity means not having access to healthy nutritious food. Besides food insecurity, people may be on a tight budget or not always have the time to search for new, healthy options at dinnertime. ROC Recipes aims to make healthy meal options more accessible.</p>
 
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/9igR-nqe_og" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				<div class = "youtube"><iframe width="560" height="315" src="https://www.youtube.com/embed/9igR-nqe_og" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
 
 				<p>ROC Recipes allows users to input a budget, or certain ingredients they have on hand, and different recipes will be generated that uses those ingredients. The recipe and any other  ingredients necessary will be provided along with a recipe. The recipes will be quick and easy to make and provides plenty of options for any given ingredient.</p>
 				<p>For people with certain dietary restrictions, the recipes will be filtered to avoid the use of any allergens. Dietary restrictions include vegetarian/vegan, gluten intolerance, lactose intolerance, common food allergies such as nuts, and kosher.</p>
